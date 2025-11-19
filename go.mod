aaf6681795d0a25b36e863935a5002e3dcfe8d8c
Tomáš Mráz <tomas.mraz@zona64.cz>
2025-11-19 15:00:13 +0100

    init data

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tomas-mraz/vulkan-asch
 
 go 1.24.10
+
+require (
+	github.com/tomas-mraz/vulkan v0.0.0-20250718111449-7cdec55a4f76
+	github.com/xlab/linmath v0.0.0-20220922225318-40b6290c3b40
+)
---
+++ helpers.go
@@ -0,0 +1,28 @@
+package asch
+
+import (
+	"log"
+	"unsafe"
+
+	vk "github.com/tomas-mraz/vulkan"
+)
+
+func check(ret vk.Result, name string) bool {
+	if err := vk.Error(ret); err != nil {
+		log.Println("[WARN]", name, "failed with", err)
+		return true
+	}
+	return false
+}
+
+func repackUint32(data []byte) []uint32 {
+	buf := make([]uint32, len(data)/4)
+	vk.Memcopy(unsafe.Pointer((*sliceHeader)(unsafe.Pointer(&buf)).Data), data)
+	return buf
+}
+
+type sliceHeader struct {
+	Data uintptr
+	Len  int
+	Cap  int
+}
